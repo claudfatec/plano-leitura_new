1e506b633be3c87e9a1311b31f61ade005134ab4
CLAUDIO ROBERTO DE CARVALHO <92130393+claudfatec@users.noreply.github.com>
2022-08-21 23:46:22 -0300

    link to VM.

--- src/componentes/Loading.tsx
+++ src/componentes/Loading.tsx
@@ -7,7 +7,7 @@ export const Loading = (props: Props) => {
     return (
         <div>
             <ButtonGroup>
-                <Button>Sl 144</Button>
+                <Button><a href='http://claudfatec.westeurope.cloudapp.azure.com'>Sl 144</a></Button>
                 <Button>Sl 145</Button>
                 <Button>Sl 146</Button>
                 <Button>Sl 147</Button>
